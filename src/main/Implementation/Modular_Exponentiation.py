7b4f451d0f48eab43f4d89275589340378ec6ff9
sini3794 <sini3794@student.su.se>
2025-01-09 16:05:12 +0100

    addition fixes

--- src/main/Implementation/Modular_Exponentiation.py
+++ src/main/Implementation/Modular_Exponentiation.py
@@ -44,74 +44,70 @@ def full_adder(circuit,a,b,r,c_in,c_out,aux):
     # circuit.measure(c_out,0)
     circuit.barrier()
 
-
+# Addition function
+# adds two values made with n number of qubits
+# prams:
+# circuit: Quantumcircuit
+# a: list of qubit in size n
+# b: list of qubit in size n
+# r: list of qubit in size n
+# aux: list of qubits with minimum size 4
 def addition(circuit, a, b, r, aux):
+    def carry_over(circuit, a, b, c_in, c_out):
+        circuit.ccx(a,b, c_out)
+        circuit.cx(a,b)
+        circuit.ccx(b,c_in,c_out)
+        circuit.cx(a,b)
+        circuit.barrier()
+    
     for i in reversed(range(len(a))):
+        # adds the qubits at position i in a and b with switching c_in and c_out
         if i % 2 == 0:
             full_adder(circuit, a[i], b[i], r[i], aux[0], aux[1], aux[2])
         else:
             full_adder(circuit, a[i], b[i], r[i], aux[1], aux[0], aux[2])
+        # makes the previous c_out (which is the current c_in)
         for n in range(len(a)-i):
             if n == 0:
                 circuit.barrier()
             else:
                 if n % 2 == 0:
-                    c_aux = aux[2]
-                    c_va = aux[3]
+                    c_in = aux[2]
+                    c_out = aux[3]
                 else:
-                    c_aux = aux[3]
-                    c_va = aux[2]
-                circuit.ccx(a[n],b[n], c_va)
-                circuit.cx(a[n],b[n])
-                circuit.ccx(b[n],c_aux,c_va)
-                circuit.cx(a[n],b[n])
-                circuit.barrier()
+                    c_in = aux[3]
+                    c_out = aux[2]
+                carry_over(circuit, a[n], b[n], c_in, c_out)
+        # sets c_in to the previous c_out negating it an clearing the aux register qubit
         if i % 2 == 0:
             circuit.cx(aux[3], aux[0])
         else:
             circuit.cx(aux[2], aux[1])
         circuit.barrier()
+        #reverse the makeing of the previous c_out clearing the used aux register qubits
         for n in reversed(range(len(a)-i)):
             if n == 0:
                 circuit.barrier()
             else:
                 if n % 2 == 0:
-                    c_aux = aux[2]
-                    c_va = aux[3]
+                    c_in = aux[2]
+                    c_out = aux[3]
                 else:
-                    c_aux = aux[3]
-                    c_va = aux[2]
-                circuit.cx(a[n],b[n])
-                circuit.ccx(b[n],c_aux,c_va)
-                circuit.cx(a[n],b[n])
-                circuit.ccx(a[n],b[n], c_va)
-                circuit.barrier()
-    for i in range(len(a)-1):
-        if i % 2 == 0:
-            circuit.cx(a[i],b[i])
-            circuit.ccx(b[i],aux[0],aux[1])
-            circuit.cx(a[i],b[i])
-            circuit.ccx(a[i],b[i], aux[1])
-            circuit.barrier()
+                    c_in = aux[3]
+                    c_out = aux[2]
+                carry_over(circuit, a[n], b[n], c_in, c_out)
+    # clearing the last c_out by making the inverse carry over operations
+    for i in range(len(a)):
+        if i % 2 != 0:
+            carry_over(circuit, a[i], b[i], aux[0], aux[1])
         else:
-            circuit.cx(a[i],b[i])
-            circuit.ccx(b[i],aux[1],aux[0])
-            circuit.cx(a[i],b[i])
-            circuit.ccx(a[i],b[i], aux[0])
-            circuit.barrier()
+            carry_over(circuit, a[i], b[i], aux[1], aux[0])
     circuit.barrier()
-                    
-                
-            
-        
-        
-        
-        
-        
 
 
-
-############################## Simulation
+##################################################################
+#                           Simulation
+##################################################################
 
 def basic_simulation(circuit):
     
@@ -137,14 +133,14 @@ aux = QuantumRegister(4,"AUX")
 c_bits = ClassicalRegister(4)
 circuit = QuantumCircuit(a,b,r,aux,c_bits)
 set_bits(circuit, a, "111")
-set_bits(circuit, b, "101")
+set_bits(circuit, b, "111")
 # circuit.x(aux[0])
 # full_adder(circuit,a,b,r,aux[0],aux[1],aux[2])
 addition(circuit, a, b, r, aux)
 
 ##mesure if aux is empty
 
-# circuit.measure(aux, [3,2,1,0])
+circuit.measure(aux, [3,2,1,0])
 # for n, i in zip(reversed(range(r._size)), range(r._size)):
 #     circuit.measure(r[i], n)
 print(circuit)
