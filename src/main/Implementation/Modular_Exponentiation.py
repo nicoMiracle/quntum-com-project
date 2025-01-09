8c49391e433fc061053b78d5900ecb02911fa591
sini3794 <sini3794@student.su.se>
2025-01-09 15:36:28 +0100

    addition test needed

--- src/main/Implementation/Modular_Exponentiation.py
+++ src/main/Implementation/Modular_Exponentiation.py
@@ -7,7 +7,7 @@ from qiskit . providers . basic_provider import BasicSimulator
 def set_bits(circuit, a, x):
     for i in reversed(range(len(a))):
         if x[i] == "1":
-            circuit.x(a[1])
+            circuit.x(a[i])
 
 #the copy function 1.2
 def copy(circuit, A, B):
@@ -46,28 +46,60 @@ def full_adder(circuit,a,b,r,c_in,c_out,aux):
 
 
 def addition(circuit, a, b, r, aux):
-    for i in range(len(a)):
+    for i in reversed(range(len(a))):
         if i % 2 == 0:
             full_adder(circuit, a[i], b[i], r[i], aux[0], aux[1], aux[2])
         else:
             full_adder(circuit, a[i], b[i], r[i], aux[1], aux[0], aux[2])
-        circuit.barrier()
-        
-        for n in reversed(range(i+1)):
+        for n in range(len(a)-i):
             if n == 0:
-                break
+                circuit.barrier()
             else:
                 if n % 2 == 0:
-                    c_in = aux[0]
-                    c_out = aux[2]
+                    c_aux = aux[2]
+                    c_va = aux[3]
                 else:
-                    c_in = aux[2]
-                    c_out = aux[0]
-                circuit.ccx(a[n],b[n], c_out)
+                    c_aux = aux[3]
+                    c_va = aux[2]
+                circuit.ccx(a[n],b[n], c_va)
                 circuit.cx(a[n],b[n])
-                circuit.ccx(b[n],c_in,c_out)    
+                circuit.ccx(b[n],c_aux,c_va)
                 circuit.cx(a[n],b[n])
                 circuit.barrier()
+        if i % 2 == 0:
+            circuit.cx(aux[3], aux[0])
+        else:
+            circuit.cx(aux[2], aux[1])
+        circuit.barrier()
+        for n in reversed(range(len(a)-i)):
+            if n == 0:
+                circuit.barrier()
+            else:
+                if n % 2 == 0:
+                    c_aux = aux[2]
+                    c_va = aux[3]
+                else:
+                    c_aux = aux[3]
+                    c_va = aux[2]
+                circuit.cx(a[n],b[n])
+                circuit.ccx(b[n],c_aux,c_va)
+                circuit.cx(a[n],b[n])
+                circuit.ccx(a[n],b[n], c_va)
+                circuit.barrier()
+    for i in range(len(a)-1):
+        if i % 2 == 0:
+            circuit.cx(a[i],b[i])
+            circuit.ccx(b[i],aux[0],aux[1])
+            circuit.cx(a[i],b[i])
+            circuit.ccx(a[i],b[i], aux[1])
+            circuit.barrier()
+        else:
+            circuit.cx(a[i],b[i])
+            circuit.ccx(b[i],aux[1],aux[0])
+            circuit.cx(a[i],b[i])
+            circuit.ccx(a[i],b[i], aux[0])
+            circuit.barrier()
+    circuit.barrier()
                     
                 
             
@@ -101,19 +133,19 @@ b = QuantumRegister(size_num,"b")
 # c_in = QuantumRegister(1,"c_in")
 # c_out = QuantumRegister(1,"c_out")
 r = QuantumRegister(size_num,"r")
-aux = QuantumRegister(3,"AUX")
-c_bits = ClassicalRegister(3)
+aux = QuantumRegister(4,"AUX")
+c_bits = ClassicalRegister(4)
 circuit = QuantumCircuit(a,b,r,aux,c_bits)
-set_bits(circuit, a, "101")
-set_bits(circuit, b, "001")
+set_bits(circuit, a, "111")
+set_bits(circuit, b, "101")
 # circuit.x(aux[0])
 # full_adder(circuit,a,b,r,aux[0],aux[1],aux[2])
 addition(circuit, a, b, r, aux)
 
-#mesure if aux is empty
-# for n in range(aux._size):
-#     circuit.measure(aux[n], n)
-for n in range(r._size):
-    circuit.measure(r[n], n)
+##mesure if aux is empty
+
+# circuit.measure(aux, [3,2,1,0])
+# for n, i in zip(reversed(range(r._size)), range(r._size)):
+#     circuit.measure(r[i], n)
 print(circuit)
 basic_simulation(circuit)
--- src/main/test/set_tests.py
+++ src/main/test/set_tests.py
@@ -1,11 +1,15 @@
 from qiskit import QuantumCircuit
 from qiskit.providers.basic_provider import BasicSimulator
 
-from src.main.Implementation.Modular_Exponentiation import basic_simulation, set_bits
+def set_bits(circuit, a, x):
+    for i in reversed(range(len(a))):
+        if x[i] == "1":
+            circuit.x(a[i])
+
 
 def set_test():
     A = [0,1,2,3]
-    X = "1111"
+    X = "1101"
     circuit = QuantumCircuit(len(A),4)
     circuit.barrier()
     set_bits(circuit,A,X)
@@ -13,5 +17,16 @@ def set_test():
 
     print(circuit)
     basic_simulation(circuit)
+    
+    
+def basic_simulation(circuit):
+    backend = BasicSimulator ()
+    n_shots = 1024 # Default number of shots is 1024
+    result = backend . run ( circuit , shots = n_shots ) . result ()
+    # Extract counts and probability distribution
+    counts = result.get_counts()
+    prob = { key : value / n_shots for key , value in counts.items() }
+    print (" Counts : ", counts )
+    print (" Probabilities : ", prob )
     
 set_test()
