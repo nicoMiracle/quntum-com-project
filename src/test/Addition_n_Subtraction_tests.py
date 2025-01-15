35e2ada70154bb4efcf3c5c8ce661810a5d54f93
sini3794 <sini3794@student.su.se>
2025-01-15 17:32:27 +0100

    addition is fixed

--- src/test/Addition_n_Subtraction_tests.py
+++ src/test/Addition_n_Subtraction_tests.py
@@ -1,20 +1,10 @@
-#Nicole Nechita, rone8293
-
 from qiskit import QuantumCircuit,QuantumRegister, ClassicalRegister
-from qiskit . providers . basic_provider import BasicSimulator
+from qiskit.providers.basic_provider import BasicSimulator
 
-#the set_bits function 1.1
 def set_bits(circuit, a, x):
     for i in reversed(range(len(a))):
         if x[i] == "1":
             circuit.x(a[i])
-
-#the copy function 1.2
-def copy(circuit, A, B):
-    amount_registers = len(A)
-    for i in range(amount_registers):
-        circuit.cx(A[i],B[i])
-        circuit.barrier()
 
 def full_adder(circuit,a,b,r,c_in,c_out,aux):
     #a XOR b XOR c_in -> r
@@ -38,21 +28,26 @@ def full_adder(circuit,a,b,r,c_in,c_out,aux):
     circuit.cx(a,aux)
     circuit.barrier()
 
+   #first bit is r, second is carry_out when measured
+   #examplecounts 01 means r came out 0 and c_out is 1
+    # circuit.measure(r,1)
+    # circuit.measure(c_out,0)
+    circuit.barrier()
+
 # Addition function
 # adds two values made with n number of qubits
 #
-# TODO: need to make this circuit into one of those fancy compact one 
-# otherwise we are gonna be scrolling for days whe we do multiplication
+# aux[0] will always be the first carry in of the addition
+#
+# TODO: need to make this circuit into one of those fancy compact ones
+# otherwise we are gonna be scrolling for days when we do multiplication
 #
 # prams:
 # circuit: QuantumCircuit
 # a: list of qubit in size n
 # b: list of qubit in size n
 # r: list of qubit in size n
-# aux: list of qubits with minimum size n + 3
-
-
-
+# aux: list of qubits with minimum size 4
 def addition(circuit, a, b, r, aux):
     def carry_over(circuit, a, b, c_in, c_out):
         circuit.ccx(a,b, c_out)
@@ -122,73 +117,80 @@ def addition(circuit, a, b, r, aux):
     circuit.cx(aux[0], aux[3])
     circuit.barrier()
 
-# havent test so dont know if this works
 def subtraction(circuit, a, b, r, aux):
     circuit.x(b)
-    circuit.x(aux[0]) ## need to check %2 for 
+    circuit.x(aux[0]) 
     circuit.barrier()
     addition(circuit, a, b, r, aux)
     circuit.x(b)
     circuit.x(aux[0])
     circuit.barrier()
 
-##################################################################
-#                           Simulation
-##################################################################
+def add_test(value_a, value_b, expected):
+    size_num = len(expected)
+    a = QuantumRegister(size_num,"a")
+    b = QuantumRegister(size_num,"b")
+    # c_in = QuantumRegister(1,"c_in") # needed to remove these for testing of addition
+    # c_out = QuantumRegister(1,"c_out")
+    r = QuantumRegister(size_num,"r")
+    aux = QuantumRegister(size_num+3,"AUX")
+    c_bits = ClassicalRegister(size_num)
+    circuit = QuantumCircuit(a,b,r,aux,c_bits)
+    set_bits(circuit, a, value_a)
+    set_bits(circuit, b, value_b)
+    addition(circuit, a, b, r, aux)
 
-def basic_simulation(circuit):
+    for n, i in zip(reversed(range(r._size)), range(r._size)):
+        circuit.measure(r[i], n)
+
+    print("Expected: " + expected)
+    basic_simulation(circuit)
     
+def sub_test(value_a, value_b, expected):
+    size_num = len(expected)
+    a = QuantumRegister(size_num,"a")
+    b = QuantumRegister(size_num,"b")
+    # c_in = QuantumRegister(1,"c_in") # needed to remove these for testing of addition
+    # c_out = QuantumRegister(1,"c_out")
+    r = QuantumRegister(size_num,"r")
+    aux = QuantumRegister(size_num+3,"AUX")
+    c_bits = ClassicalRegister(size_num)
+    circuit = QuantumCircuit(a,b,r,aux,c_bits)
+    set_bits(circuit, a, value_a)
+    set_bits(circuit, b, value_b)
+    subtraction(circuit, a, b, r, aux)
+
+    for n, i in zip(reversed(range(r._size)), range(r._size)):
+        circuit.measure(r[i], n)
+
+    print("Expected: " + expected)
+    basic_simulation(circuit)
+    
+    
+def basic_simulation(circuit):
     backend = BasicSimulator ()
-    n_shots = 1024 # Default number of shots is 1024
+    n_shots = 10 # Default number of shots is 1024
     result = backend . run ( circuit , shots = n_shots ) . result ()
     # Extract counts and probability distribution
     counts = result.get_counts()
     prob = { key : value / n_shots for key , value in counts.items() }
-    print (" Counts : ", counts )
     print (" Probabilities : ", prob )
-    
-def aer_simulation(circuit):
-    # Import the transpile module
-    from qiskit import transpile
-    # Define the backend AerSimulator
-    from qiskit_aer import AerSimulator
-    backend = AerSimulator ()
-    # Transpile the circuit to a set of gates
-    # compatible with the backend
-    compiled_circuit = transpile ( circuit , backend )
-    # Execute the circuit on the qasm simulator .
-    n_shots = 1024 # default number of shots .
-    job_sim = backend . run ( compiled_circuit , shots = n_shots )
-    # Extract Results
-    result_sim = job_sim . result ()
-    counts = result_sim . get_counts ( compiled_circuit )
-    probs = { key : value / n_shots for key , value in counts . items () }
-    print (" Counts ", counts )
-    print (" Probabilities :", probs )
 
-
-
-size_num = 5
-
-a = QuantumRegister(size_num,"a")
-b = QuantumRegister(size_num,"b")
-# c_in = QuantumRegister(1,"c_in") # needed to remove these for testing of addition
-# c_out = QuantumRegister(1,"c_out")
-r = QuantumRegister(size_num,"r")
-aux = QuantumRegister(8,"AUX")
-c_bits = ClassicalRegister(8)
-circuit = QuantumCircuit(a,b,r,aux,c_bits)
-set_bits(circuit, a, "111111")
-set_bits(circuit, b, "001001")
-# circuit.x(aux[0])
-# full_adder(circuit,a,b,r,aux[0],aux[1],aux[2])
-addition(circuit, a, b, r, aux)
-
-##mesure if aux is empty
-
-circuit.measure(aux, [7,6,5,4,3,2,1,0])
-# for n, i in zip(reversed(range(r._size)), range(r._size)):
-#     circuit.measure(r[i], n)
-print(circuit)
-basic_simulation(circuit)
-# aer_simulation(circuit)
+print("Addition tests:")
+add_test("0001", "0001", "0010")
+add_test("0000", "0000", "0000")
+add_test("0001", "1111", "0000")
+add_test("0101", "0001", "0110")
+add_test("10001", "00001", "10010")
+add_test("11111", "11111", "11110")
+add_test("00001", "11111", "00000")
+add_test("00101", "00001", "00110")
+print("Subtraction tests:")
+sub_test("0001", "0001", "0000")
+sub_test("0000", "0000", "0000")
+sub_test("1111", "0001", "1110")
+sub_test("0101", "0001", "0100")
+sub_test("10001", "00001", "10000")
+sub_test("11111", "11111", "00000")
+sub_test("01111", "00001", "01110")
+sub_test("10101", "10001", "00100")
---
+++ src/test/adder_test.py
@@ -0,0 +1,63 @@
+from qiskit import QuantumCircuit,QuantumRegister, ClassicalRegister
+from qiskit . providers . basic_provider import BasicSimulator
+
+#the set_bits function 1.1
+def set_bits(circuit, a, x):
+    for i in reversed(range(len(a))):
+        if x[i] == "1":
+            circuit.x(a[i])
+
+def full_adder(circuit,a,b,r,c_in,c_out,aux):
+    #a XOR b XOR c_in -> r
+    #AUX is used to store a XOR b
+    circuit.cx(a,aux)
+    circuit.cx(b,aux)
+    circuit.cx(aux,r)
+    circuit.cx(c_in,r)
+    circuit.barrier()
+
+    #do gates for carry_out
+    circuit.ccx(a,b,c_out)
+    circuit.cx(a,b)
+    circuit.ccx(b,c_in,c_out)
+    circuit.cx(a,b)
+    circuit.barrier()
+
+    #reset AUX by doing the operation again
+    
+    circuit.cx(b,aux)
+    circuit.cx(a,aux)
+    circuit.barrier()
+    
+def add_test(value_a, value_b, expected):
+    a = QuantumRegister(1,"a")
+    b = QuantumRegister(1,"b")
+    c_in = QuantumRegister(1,"c_in") # needed to remove these for testing of addition
+    c_out = QuantumRegister(1,"c_out")
+    r = QuantumRegister(1,"r")
+    aux = QuantumRegister(1,"AUX")
+    c_bits = ClassicalRegister(2)
+    circuit = QuantumCircuit(a,b,r,c_in,c_out,aux,c_bits)
+    set_bits(circuit, a, value_a)
+    set_bits(circuit, b, value_b)
+    full_adder(circuit, a, b, r, c_in, c_out, aux)
+    circuit.measure(r,1)
+    circuit.measure(c_out,0)
+
+    print("Expected: " + expected)
+    basic_simulation(circuit)
+    
+    
+def basic_simulation(circuit):
+    backend = BasicSimulator ()
+    n_shots = 1024 # Default number of shots is 1024
+    result = backend . run ( circuit , shots = n_shots ) . result ()
+    # Extract counts and probability distribution
+    counts = result.get_counts()
+    prob = { key : value / n_shots for key , value in counts.items() }
+    print (" Probabilities : ", prob )
+    
+add_test("1", "1", "01")
+add_test("0", "0", "00")
+add_test("1", "0", "10")
+add_test("0", "1", "10")
