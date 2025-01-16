e8bd95359607f5cb96e10d32366beccb0411589f
sini3794 <sini3794@student.su.se>
2025-01-16 13:32:07 +0100

    addition fix

--- src/main/Implementation/Modular_Exponentiation.py
+++ src/main/Implementation/Modular_Exponentiation.py
@@ -41,95 +41,35 @@ def full_adder(circuit,a,b,r,c_in,c_out,aux):
 # Addition function
 # adds two values made with n number of qubits
 #
-# TODO: need to make this circuit into one of those fancy compact one 
-# otherwise we are gonna be scrolling for days whe we do multiplication
+# aux[1] will always be the first carry in of the addition
 #
 # prams:
 # circuit: QuantumCircuit
 # a: list of qubit in size n
 # b: list of qubit in size n
 # r: list of qubit in size n
-# aux: list of qubits with minimum size n + 3
-
-
-
-def addition(circuit, a, b, r, aux):
-    def carry_over(circuit, a, b, c_in, c_out):
-        circuit.ccx(a,b, c_out)
-        circuit.cx(a,b)
-        circuit.ccx(b,c_in,c_out)
-        circuit.cx(a,b)
-        circuit.barrier()
-    
-    # copies the first c_in in aux[3] and switched first c_in to aux[1] if nececary
-    circuit.cx(aux[0], aux[3])
-    if len(a) % 2 == 0:
-        circuit.cx(aux[0], aux[1])
-        circuit.cx(aux[1], aux[0])
-    circuit.barrier()
-    
+# aux: list of qubits with minimum size of n+2
+def addition(circuit, a, b, r, aux):    
     for i in reversed(range(len(a))):
-        # adds the qubits at position i in a and b with switching c_in and c_out
-        
-        if i % 2 == 0:
-            carry_in = aux[0] 
-            carry_out = aux[1]
-        else:
-            carry_in = aux[1] 
-            carry_out = aux[0]
-            
-        # circuit.append(qc, [a[i], b[i], r[i], carry_in, carry_out, aux[2]])
-        full_adder(circuit, a[i], b[i], r[i], carry_in, carry_out, aux[2])
-        # makes the previous c_out (which is the current c_in)
-        switch_out = None
-        for n, ax in zip(reversed(range(len(a)-i)), range(len(a)-i)):
-            # circuit.barrier()
-            if n == 0:
-                circuit.barrier()
-            else:
-                c_in = aux[ax+3]
-                c_out = aux[ax+4]
-                carry_over(circuit, a[i+n], b[i+n], c_in, c_out)
-                switch_out = c_out
-        # sets c_in to the previous c_out negating it an clearing the aux register qubit
-        if switch_out != None:
-            circuit.cx(switch_out, carry_in)
+        _p = len(a)-i
+        full_adder(circuit, a[i], b[i], r[i], aux[_p], aux[_p+1], aux[0])
+    for i in range(len(a)):
+        _p = len(a)-i
+        circuit.ccx(a[i],b[i], aux[_p+1])
+        circuit.cx(a[i],b[i])
+        circuit.ccx(b[i],aux[_p],aux[_p+1])
+        circuit.cx(a[i],b[i])
         circuit.barrier()
-        #reverse the makeing of the previous c_out clearing the used aux register qubits
-        for ax, n in zip(reversed(range(len(a)-i)), range(len(a)-i)):
-            if n == 0:
-                circuit.barrier()
-            else:
-                c_in = aux[ax+3]
-                c_out = aux[ax+4]
-                carry_over(circuit, a[i+n], b[i+n], c_in, c_out)
-    # clearing the last c_out by making the inverse carry over operations
-    switch = None
-    for i, ax in zip(reversed(range(len(a)-1)), range(len(a)-1)):
-        carry_over(circuit, a[i+1], b[i+1], aux[ax+3], aux[ax+4])
-        switch = aux[ax+4]
-    if switch != None:
-        circuit.cx(switch,carry_out)
-    circuit.barrier()
-    for ax, i in zip(reversed(range(len(a)-1)), range(len(a)-1)):
-        carry_over(circuit, a[i+1], b[i+1], aux[ax+3], aux[ax+4])
-        switch = aux[ax+4]
-    circuit.barrier()
-    # resets the aux for c_in
-    if len(a) % 2 == 0:
-        circuit.cx(aux[0], aux[1])
-        circuit.cx(aux[1], aux[0])
-    circuit.cx(aux[0], aux[3])
     circuit.barrier()
 
 # havent test so dont know if this works
 def subtraction(circuit, a, b, r, aux):
     circuit.x(b)
-    circuit.x(aux[0]) ## need to check %2 for 
+    circuit.x(aux[1])  
     circuit.barrier()
     addition(circuit, a, b, r, aux)
     circuit.x(b)
-    circuit.x(aux[0])
+    circuit.x(aux[1])
     circuit.barrier()
 
 ##################################################################
